cbb699ffd6330170514f0a9022531ecda8460c30
Who <56252622+DaDDy-chilll@users.noreply.github.com>
2022-06-27 15:10:52 +0630

    setup finish setup finish

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
+require("dotenv").config();
 const express = require("express");
+const ejs = require("ejs");
+const bdparser = require("body-parser");
+const mongo = require("mongoose");
 
 const app = express();
+
+app.set("view engine", "ejs");
+app.use(bdparser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("Server is active");
 });
 
-app.listen(8080, function () {
-  console.log("Server is running on Port:3000");
+app.listen(process.env.PORT, function () {
+  console.log(`Server is running on Port:${process.env.PORT}`);
 });
