b7d59f79ec8f79b8a4d28de7d15d04d30729f4cd
Who <blacklistuser420@gmail.com>
2022-07-01 05:13:49 +0000

    login form done

--- app.js
+++ app.js
@@ -12,13 +12,23 @@ app.use(bdparser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render('login',{loginHead:'Login',btn_switch:'SignUp',btn_value:'signup'});
+  res.render('login',{loginHead:'login',btn_switch:'SignUp',btn_value:'signup'});
 });
-
+app.post('/login',(req,res)=>{
+  const userEmail = req.body.email;
+  const userPass = req.body.pass;
+  res.send(`this login post route Email:${userEmail} and Pass:${userPass}`);
+})
 
 app.get('/signup',(req,res)=>{
-  res.render('login',{loginHead:'SignUp',btn_switch:'Login',btn_value:''});
+  res.render('login',{loginHead:'signup',btn_switch:'Login',btn_value:''});
 });
+
+app.post('/signup',(req,res)=>{
+  const userEmail = req.body.email;
+  const userPass = req.body.pass;
+  res.send(`this signup route with Email:${userEmail} and Pass:${userPass}`);
+})
 
 app.listen(process.env.PORT, function () {
   console.log(`Server is running on Port:${process.env.PORT}`);
